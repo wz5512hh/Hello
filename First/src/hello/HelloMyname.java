25f23ae17baad84701d263ed1802379d6da3451c
admin <admin@DESKTOP-R2CTUKC>
2020-08-03 11:18:02 +0900

    수정했음

---
+++ First/src/hello/HelloMyname.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HelloMyname {
+
+	public static void main(String[] args) {
+		System.out.println("헬로우 월드!");
+
+	}
+
+}
---
+++ First/src/hello/Hellojava.java
@@ -0,0 +1,22 @@
+/*
+ * Date : 2020.06.26
+ * Author : 최혜린
+ * 설명 : 첫 번째 자바 프로그램 입니다.
+ * Version : 1.0
+ */
+
+package hello;
+
+public class Hellojava {
+
+public static void main(String[] args) {
+	// 주석. 코드설명. 개발자가 보기 위함
+	System.out.println("켁, 븜");
+	/* 이것은
+  	      여러줄
+	      주석
+	      */
+
+}
+	
+	}
