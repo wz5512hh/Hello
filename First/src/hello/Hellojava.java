91d31a4c54e0d86684e0436019fee69632f2a2ec
admin <admin@DESKTOP-R2CTUKC>
2020-08-03 11:41:15 +0900

    chang

--- First/src/hello/Hellojava.java
+++ First/src/hello/Hellojava.java
@@ -13,6 +13,7 @@ public static void main(String[] args) {
 	// 주석. 코드설명. 개발자가 보기 위함
 	System.out.println("켁, 븜");
 	System.out.println("ㅇㅇ");
+	System.out.println("ss");
 	/* 이것은
   	      여러줄
 	      주석
